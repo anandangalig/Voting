29766bf73c660841cc31718e58bb43e80dd261fb
Anand Angalig and Rebecca Zarsky <anand@>
2016-08-11 09:24:17 -0400

    create branch statement for scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  // $(".numeric").numeric();
+  $("form#age").submit(function(event) {
+  event.preventDefault();
+  var ageInput = parseInt($("#age").val());
+  if (ageInput >= 18) {
+    $('#vote').show();
+  } else {
+    $('#noVote').show();
+  }
+  });
 });
